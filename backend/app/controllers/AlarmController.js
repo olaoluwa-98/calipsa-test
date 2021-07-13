bcfcf0665b33ea0de3ae0d88e6304fe58a091096
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2021-07-13 01:47:03 +0100

    chore: add filtering by processing result to alarms endpoint

--- backend/app/controllers/AlarmController.js
+++ backend/app/controllers/AlarmController.js
@@ -16,7 +16,7 @@ class AlarmController {
    * @returns Promise<Object>
    */
   static async getAlarms(req, res) {
-    const { page = 1, pageSize = 10, timestamp_from, timestamp_to = new Date() } = req.query;
+    const { page = 1, pageSize = 10, timestamp_from, timestamp_to = new Date(), outcome } = req.query;
     if (pageSize > 200) throw new BadRequestException("pageSize should be at most 200");
     const data = await Alarm.query(qb => {
       if (timestamp_from) {
@@ -25,12 +25,14 @@ class AlarmController {
       if (timestamp_to) {
         qb.where("timestamp", "<=", new Date(timestamp_to));
       }
+      if (outcome != null && (outcome == "1" || outcome == "true")) qb.where("outcome", "=", 1);
+      else if (outcome != null && (outcome == "0" || outcome == "false")) qb.where("outcome", 0);
     }).fetchPage({
       pageSize,
       page,
       withRelated: ["location"]
     });
-    
+
     return res.json({
       status: "success",
       message: "Alarms retrieved",
